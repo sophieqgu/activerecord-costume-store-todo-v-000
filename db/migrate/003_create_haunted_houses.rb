78025cb0a63f9abd62f3dd0fed1b5b39a03cfdeb
Sophie Gu <sophieqgu@gmail.com>
2019-06-26 02:54:17 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,16 @@
 # Create your haunted_houses migration here
+
+class CreateHuntedHouses < ActiveRecord::Migration[5.1]
+  def change 
+    create_table :haunted_houses do |t|
+      t.string :name 
+      t.string :location
+      t.string :theme 
+      t.integer :price 
+      t.boolean :family_friendly 
+      t.string :image_url
+      t.datetime :created_at
+      t.datetime :updated_at 
+    end 
+  end 
+end 
