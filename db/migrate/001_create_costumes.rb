43a0eaf0d79287d6422f12b0717f2af8bd85e36a
Sophie Gu <sophieqgu@gmail.com>
2019-06-26 02:51:31 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -15,4 +15,8 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
       t.integer :price 
       t.integer :size 
       t.string :image_url
-      t.timestamp :
+      t.datetime :created_at
+      t.datetime :updated_at 
+    end 
+  end 
+end 
